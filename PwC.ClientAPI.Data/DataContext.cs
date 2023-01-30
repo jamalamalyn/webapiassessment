fd40569667698c2dad34a2a10b3762f0835c5c15
brendanlrelab <brendan@relab.co.nz>
2023-01-30 21:10:27 +1300

    Inifial & Final commit - Wrote web api with controller end points - Added configuration - Added Entity framework context - Added interfaces & contrete implementations needed for CRUD in repository pattern - Added Logging - Write controller end - Wrote Test Project using xUnit framework - opted for In-memory database for testing

---
+++ PwC.ClientAPI.Data/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PwC.ClientAPI.Domain.Models;
+using System.Linq;
+using System;
+using PwC.ClientAPI.Domain.Interfaces;
+
+namespace PwC.ClientAPI.Domain
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Client> Clients { get; set; }
+
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is Client && (
+                        e.State == EntityState.Added
+                        || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                ((Client)entityEntry.Entity).UpdateDate = DateTime.Now;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((Client)entityEntry.Entity).CreateDate = DateTime.Now;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+    }
+}
+
---
+++ PwC.ClientAPI.Domain/Models/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PwC.ClientAPI.Domain.Models
+{
+    public class Entity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public DateTime CreateDate { get; set; }
+
+        [JsonIgnore]
+        public DateTime UpdateDate { get; set; }
+    }
+}
